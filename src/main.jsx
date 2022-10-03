d0f04852335614446f5c20e9e8d0ac3f841824b4
galiardi <galiardi.dev@gmail.com>
2022-10-03 15:16:24 -0300

    Learning useRef

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import ReactDOM from "react-dom/client";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import HooksApp from "./HooksApp";
-import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
-  <MultipleCustomHooks />
+  <FocusScreen />
   //</React.StrictMode>
 );
