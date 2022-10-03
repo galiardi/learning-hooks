f4151e5e24d26308312380868db17b79468befb0
galiardi <galiardi.dev@gmail.com>
2022-10-03 20:34:37 -0300

    Learning useLayoutEffect

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,12 @@ import ReactDOM from "react-dom/client";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import HooksApp from "./HooksApp";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
-import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
+import { Layout } from "./05-useLayoutEffect/Layout";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
-  <FocusScreen />
+  <Layout />
   //</React.StrictMode>
 );
